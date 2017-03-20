8d89db9406149c981b6f1691539a3258e298c34a
CamD67 <cameron.doane@gmail.com>
2017-03-19 17:04:22 -0700

    add changing images, smooth scroll

--- js/index.js
+++ js/index.js
@@ -1,7 +1,65 @@
-$(function(){
+"use strict";
 
+$(function(){
     // timeout for notification banner
     setTimeout(function(){
         $(".notification-banner").slideUp();
     }, 5000);
+
+    $("#topLink").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#top").offset().top
+        }, 1000);
+    });
+    $("#projectsLink").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#projects").offset().top - 25
+        }, 1000);
+    });
+    $("#schoolLink").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#school").offset().top - 25
+        }, 1000);
+    });
+    $("#linksLink").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#links").offset().top - 25
+        }, 1000);
+    });
+
+    function initCarousels(){
+        var allCarousels = $(".carousel");
+        for(var i = 0; i < allCarousels.length; i++){
+            (function(){
+                var currCarousel = allCarousels.eq(i);
+                currCarousel.hover(
+                    function(){
+                        currCarousel.children("span").show();
+                }, function(){
+                    currCarousel.children("span").hide();
+                });
+                var totalImages = currCarousel.children("img").length;
+                displayCarousel(currCarousel);
+                currCarousel.click(function() {
+                    var last = currCarousel.data("carouselCurrent");
+                    var next = ++last >= totalImages ? 0 : last;
+                    currCarousel.data("carouselCurrent", next);
+                    displayCarousel(currCarousel);
+                });
+            })();
+        }
+    }
+
+    function displayCarousel(carousel){
+        var currentImage = carousel.data("carouselCurrent");
+        var children = carousel.children("img");        
+        for(var i = 0; i < children.length; i++){
+            if(i === currentImage){
+                children.eq(i).show();
+            } else {
+                children.eq(i).hide();
+            }
+        }
+    }
+    initCarousels();
 });
